38a3c7aa39698d790726e34bba52e92c84384780
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2021-12-21 20:46:05 -0800

    Added UpdateNumber, UpdateOperator,Calculate,and RefreshDisplay functions

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-function add(a, b) {
+let leftNum, rightNum = '';
+let currentOperator;
+
+function UpdateNumber(selected) {
+    if (currentOperator == undefined) leftNum = leftNum + selected;
+    else rightNum = rightNum + selected;
+}
+
+function UpdateOperator(selected) {
+    if (currentOperator != undefined && rightNum !== '') {
+        Calculate();
+    }
+    currentOperator = selected;
+}
+function Calculate() {
+    leftNum = currentOperator(leftNum, rightNum);
+    currentOperator = undefined;
+    rightNum = '';
+}
+function RefreshDisplay() {
+    console.log(`refresh the display with : ${leftNum} + ${rightNum}`);
+}
+
+
+
+function Add(a, b) {
     return a + b;
 }
-function subtract(a, b) {
+function Subtract(a, b) {
     return a - b;
 }
-function divide(a, b) {
+function Divide(a, b) {
     const roundAmount = 100000;
     return Math.round(((a / b) * roundAmount) / roundAmount);
 }
 
-function multiply(a, b) {
+function Multiply(a, b) {
     return a * b;
 }
