bbfc421d6f28af90dc882e5fd0a0f50afb7ef240
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2021-12-21 22:08:10 -0800

    attached updateoperator function to each operator button. added a currentOperatorText variable to track the string for the operator (ex"+"). And modified updateoperator to track it.
    in index.html changed some placeholder values for testing

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-let leftNum, rightNum = '';
+let leftNum = '';
+let rightNum = '';
+let currentOpText = '';
 let currentOperator;
 
-const numberButtons = document.querySelectorAll(".button");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
 
-numberButtons.forEach(function (button) {
+numberButtons.forEach(button => {
     button.addEventListener("click", () => {
         UpdateNumber(button.textContent);
     });
 });
+operatorButtons.forEach(ops => {
+    let opsFunction;
+    switch (ops.textContent) {
+        case '+': opsFunction = Add; break;
+        case '-': opsFunction = Subtract; break;
+        case 'x': opsFunction = Multiply; break;
+        case '%': opsFunction = Divide; break;
+        default: alert("something went wrong! in setting operator event listeners");
+    }
+    operatorString = ops.textContent;
+    ops.addEventListener("click", () => {
+
+        UpdateOperator(opsFunction, operatorString);
+    });
+});
+
 
 
 
@@ -18,11 +37,12 @@ function UpdateNumber(selected) {
     RefreshDisplay();
 }
 
-function UpdateOperator(selected) {
+function UpdateOperator(selected, opsText) {
     if (currentOperator != undefined && rightNum !== '') {  //tests if there are already 2 terms and 1 operator and condenses it into the leftnum
         Calculate();
     }
     currentOperator = selected;
+    currentOpText = opsText;
     RefreshDisplay();
 }
 function Calculate() {
@@ -34,7 +54,9 @@ function Calculate() {
 
 //Updates the text display.
 function RefreshDisplay() {
-    console.log(`refresh the display with : ${leftNum} + ${rightNum}`);
+    currentOpText = currentOpText ? currentOpText : '';
+    console.log(`refresh the display with : ${leftNum} ${currentOpText} ${rightNum}`);
+
 }
 
 
